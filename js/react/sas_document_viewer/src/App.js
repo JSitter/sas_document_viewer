4f17a409a5af5eee135ecc8cfd8efce680269dd1
Justin Sitter <justin@jaytria.com>
2020-09-17 15:08:03 -0700

    Add React app

---
+++ js/react/sas_document_viewer/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App-header">
+            <p>This is a great React App!</p>
+        </div>
+    );
+}
+
+export default App;
---
+++ js/react/sas_document_viewer/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+ 
+const Root = () => {
+ return (
+   <App />
+ )
+}
+ 
+render(<Root/>, document.querySelector('#sas-document-viewer'));

0486c7673dd8bc8f90c210fd2d7a3ce8388b13d7
Justin Sitter <justin@jaytria.com>
2020-09-18 13:14:52 -0700

    Update config form based on Drupal.org documentation

--- src/Form/SASDocumentViewerSettingsForm.php
+++ src/Form/SASDocumentViewerSettingsForm.php
@@ -5,8 +5,8 @@
  * Contains \Drupal\sas_document_viewer\Form\SASDocumentViewerSettingsForm
  */
 namespace Drupal\sas_document_viewer\Form;
+
 use Drupal\Core\Form\ConfigFormBase;
-use Symfony\Componentj\HttpFoundation\Request;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
@@ -28,32 +28,30 @@ class SASDocumentViewerSettingsForm extends ConfigFormBase {
             'sas_document_viewer.settings'
         ];
     }
+
     /**
      * {@inheritdoc}
      */
-    public function buildForm(array $form, FormStateInterface $form_state, Request $request = NULL) {
-        $types = node_type_get_name();
+    public function buildForm(array $form, FormStateInterface $form_state) {
         $config = $this->config('sas_document_viewer.settings');
-        $form['sas_document_viewer_types'] = array(
-            '#type' => 'checkboxes',
-            '#title' => $this->t('The content types to enable sas_document_viewer on.'),
-            '#default_value' => $config->get('allowed_types'),
-            '#options' => $types,
-            '#description' => t('On the specified node types, sas document viewer option will be available and can be enables whiel that node is being edited'),
-        );
-        $form['array_filter'] = array('#type' => 'value', '#value' => TRUE);
+
+        $form['your_message'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Your message'),
+        '#default_value' => $config->get('variable_name'),
+        ];
         return parent::buildForm($form, $form_state);
+    
     }
 
     /**
      * {@inheritdoc}
      */
     public function submitForm(array &$form, FormStateInterface $form_state) {
-        $allowed_types = array_filter($form_state->getValue('sas_document_viewer_types'));
-        sort($allowed_types);
         $this->config('sas_document_viewer.settings')
-            ->set('allowed_types', $allowed_types)
-            ->save();
-            parent::submitForm($form, $form_state);
+          ->set('variable_name', $form_state->getValue('your_message'))
+          ->save();
+        parent::submitForm($form, $form_state);
     }
+  
 }

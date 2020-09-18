d6bd42f7e46ac86e21c49b941adebcdec2ae305d
Justin Sitter <justin@jaytria.com>
2020-09-17 19:27:50 -0700

    Add drupalSettings dependency

--- js/react/sas_document_viewer/src/App.js
+++ js/react/sas_document_viewer/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { render } from 'react-dom';
 
 function App() {
+    console.log(drupalSettings.sasDocumentViewer.testSetting);
     return (
         <div className="App-header">
             <p>This is a great React App!</p>
+            <p>My setting is {drupalSettings.sasDocumentViewer.testSetting}</p>
         </div>
     );
 }

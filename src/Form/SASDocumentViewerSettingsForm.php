bf5fb3d259805ecd2fc99fdae527040070d69346
Justin Sitter <justin@jaytria.com>
2020-09-19 21:59:41 -0700

    Add form fields

--- src/Form/SASDocumentViewerSettingsForm.php
+++ src/Form/SASDocumentViewerSettingsForm.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * Remember php??
+ * array_keys($contentTypes)
+ * count($contentTypes)
+ * sizeof($contentTypes)
+ * 
+ * foreach ($contentTypes as $value) {
+ *  $value
+ * }
+ * 
+ * foreach ($contentTypes as $key => $value) {
+ * $key 
+ * $value
+ * }
+ */
 
 /**
  * @file
@@ -35,11 +50,35 @@ class SASDocumentViewerSettingsForm extends ConfigFormBase {
     public function buildForm(array $form, FormStateInterface $form_state) {
         $config = $this->config('sas_document_viewer.settings');
 
-        $form['your_message'] = [
-        '#type' => 'textfield',
-        '#title' => $this->t('Your message'),
-        '#default_value' => $config->get('variable_name'),
-        ];
+        $contentTypes = $this->getContentTypes();
+        
+        foreach ($contentTypes as $key => $value) {
+            $options = $this->getFieldNames($key);
+
+            $form['enable'.$key] = [
+                '#type' => 'checkbox',
+                '#title' => $this->t('Enable browser for '.$key),
+                ];
+
+            $form[$key.'fields'] = [
+                '#type' => 'select',
+                '#title' => 'Select field to group '.$key.' by',
+                '#options' =>  $options,
+                '#description' => "Select Field",
+            ];
+
+            $form[$key.'levels'] = [
+                '#type' => 'select',
+                '#title' => 'Grouping Levels',
+                '#options' =>  array(
+                    '1' => t('1'),
+                    '2' => t('2'),
+                ),
+                '#description' => "Select number of groupings",
+            ];
+            
+        }
+        
         return parent::buildForm($form, $form_state);
     
     }
@@ -54,4 +93,28 @@ class SASDocumentViewerSettingsForm extends ConfigFormBase {
         parent::submitForm($form, $form_state);
     }
   
+    /**
+     * {@inheritdoc}
+     */
+    protected function getContentTypes() {
+        $result = \Drupal::entityTypeManager()
+            ->getStorage('node_type')
+            ->loadMultiple();
+
+        return $result;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    protected function getFieldNames($node_name) {
+        $result = \Drupal::entityManager()
+            ->getFieldDefinitions('node', $node_name);
+        $nodeArray = array();
+        foreach (array_keys($result) as $val) {
+            $nodeArray[$val] = t($val);
+        }
+
+        return $nodeArray;
+    }
 }
